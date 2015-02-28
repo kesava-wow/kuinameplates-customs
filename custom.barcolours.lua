970f714bebd1229f7df4a75edc366eac38a3be1e
kesava-wow <kesava-wow@users.noreply.github.com>
2015-02-28 03:51:13 +0000

    use numeric colour priorities

--- custom.barcolours.lua
+++ custom.barcolours.lua
@@ -2,7 +2,7 @@
 -- custom.lua for Kui_Nameplates
 -- By Kesava at curse.com
 --
--- changes colour of health bars for specific units in the table
+-- changes colour of health bars for specific units named in the in the table
 ]]
 local kn = LibStub('AceAddon-3.0'):GetAddon('KuiNameplates')
 local mod = kn:NewModule('CustomInjector', 'AceEvent-3.0')
@@ -10,11 +10,12 @@ local mod = kn:NewModule('CustomInjector', 'AceEvent-3.0')
 local units = {
     ['Stanzilla'] = { 1, 0, 0 }
 }
+local PRIORITY = 3 -- set above 10 to override tank mode colour
 
 function mod:PostShow(msg, frame)
     local mycol = units[frame.name.text]
     if mycol then
-        frame:SetHealthColour(true, unpack(mycol))
+        frame:SetHealthColour(PRIORITY, unpack(mycol))
     end
 end
 mod:RegisterMessage('KuiNameplates_PostShow', 'PostShow')
--- custom.lowhealthcolours.lua
+++ custom.lowhealthcolours.lua
@@ -1,24 +1,25 @@
 --[[
--- Kui_Nameplates
+-- custom.lua for Kui_Nameplates
 -- By Kesava at curse.com
--- All rights reserved
 --
 -- changes colour of health bars based on health percentage
--- conflicts with tank mode
 ]]
 local kn = LibStub('AceAddon-3.0'):GetAddon('KuiNameplates')
 local mod = kn:NewModule('CustomInjector', 'AceEvent-3.0')
 
 local LOW_HEALTH_COLOR = { 1, .3, .5 }
+local PRIORITY = 5 -- set above 10 to override tank mode colour
 
 local function OnHealthValueChanged(oldHealth,current)
     local frame = oldHealth:GetParent():GetParent().kui
     local percent = frame.health.percent
 
     if percent <= 20 then
-        frame:SetHealthColour(true, unpack(LOW_HEALTH_COLOR))
-    else
+        frame:SetHealthColour(PRIORITY, unpack(LOW_HEALTH_COLOR))
+        frame.stuckLowHealth = true
+    elseif frame.stuckLowHealth then
         frame:SetHealthColour(false)
+        frame.stuckLowHealth = nil
     end
 end
 
