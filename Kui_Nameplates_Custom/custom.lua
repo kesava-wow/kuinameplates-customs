b4581e939d24b424bbb5c8711a109aa6c342a77b
kesava-wow <kesava-wow@users.noreply.github.com>
2015-09-19 16:11:47 +0100

    update template

--- Kui_Nameplates_Custom/custom.lua
+++ Kui_Nameplates_Custom/custom.lua
@@ -1,36 +1,35 @@
 --[[
 -- Kui_Nameplates
 -- By Kesava at curse.com
--- All rights reserved
-
-   Some examples of simple modifications can be found at the following URL:
-   https://github.com/kesava-wow/kuinameplates-customs
-]]
+-- Some examples of simple modifications can be found at the following URL:
+-- https://github.com/kesava-wow/kuinameplates-customs
+--
+-- Other possible messages include:
+-- _PostTarget  - fired when a nameplate becomes the player's target.
+-- _GuidStored  - fired when a nameplate's GUID becomes available, through the
+--                player targeting or mousing over its frame.
+-- _GuidAssumed - fired when a nameplate with an assumed unique name is
+--                assigned a GUID.
+--
+-- To perform some action OnUpdate, you can either overload the frame's
+-- :UpdateFrame script, or hook OnUpdate as normal.
+-- (as per layout.lua, addon:InitFrame)
+--]]
 local kn = LibStub('AceAddon-3.0'):GetAddon('KuiNameplates')
 local mod = kn:NewModule('CustomInjector', 'AceEvent-3.0')
-
 ---------------------------------------------------------------------- Create --
 function mod:PostCreate(msg, frame)
-	-- Place code to be performed after a frame is created here.
+    -- Place code to be performed after a frame is created here.
 end
-
 ------------------------------------------------------------------------ Show --
 function mod:PostShow(msg, frame)
-	-- Place code to be performed after a frame is shown here.
+    -- Place code to be performed after a frame is shown here.
 end
-
 ------------------------------------------------------------------------ Hide --
 function mod:PostHide(msg, frame)
-	-- Place code to be performed after a frame is hidden here.
+    -- Place code to be performed after a frame is hidden here.
 end
-
----------------------------------------------------------------------- Target --
-function mod:PostTarget(msg, frame)
-	-- Place code to be performed when a frame becomes the player's target here.
-end
-
 -------------------------------------------------------------------- Register --
 mod:RegisterMessage('KuiNameplates_PostCreate', 'PostCreate')
 mod:RegisterMessage('KuiNameplates_PostShow', 'PostShow')
 mod:RegisterMessage('KuiNameplates_PostHide', 'PostHide')
-mod:RegisterMessage('KuiNameplates_PostTarget', 'PostTarget')
