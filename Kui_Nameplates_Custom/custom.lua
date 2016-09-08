4dfa9be1ea81b375a90635aefce400eeef744c1a
kesava-wow <kesava-wow@users.noreply.github.com>
2016-09-08 14:18:37 +0100

    update template

--- Kui_Nameplates_Custom/custom.lua
+++ Kui_Nameplates_Custom/custom.lua
@@ -1,35 +1,26 @@
 --[[
--- Kui_Nameplates
+-- Custom code injector template for Kui_Nameplates_Core
 -- By Kesava at curse.com
--- Some examples of simple modifications can be found at the following URL:
--- https://github.com/kesava-wow/kuinameplates-customs
---
--- Other possible messages include:
--- _PostTarget  - fired when a nameplate becomes the player's target.
--- _GuidStored  - fired when a nameplate's GUID becomes available, through the
---                player targeting or mousing over its frame.
--- _GuidAssumed - fired when a nameplate with an assumed unique name is
---                assigned a GUID.
---
--- To perform some action OnUpdate, you can either overload the frame's
--- :UpdateFrame script, or hook OnUpdate as normal.
--- (as per layout.lua, addon:InitFrame)
 --]]
-local addon = LibStub('AceAddon-3.0'):GetAddon('KuiNameplates')
-local mod = addon:NewModule('CustomInjector', addon.Prototype, 'AceEvent-3.0')
----------------------------------------------------------------------- Create --
-function mod:PostCreate(msg, frame)
+local folder,ns=...
+local addon = KuiNameplates
+local core = KuiNameplatesCore
+local mod = addon:NewPlugin('CustomInjector',101)
+-- create ######################################################################
+function mod:Create(frame)
     -- Place code to be performed after a frame is created here.
 end
------------------------------------------------------------------------- Show --
-function mod:PostShow(msg, frame)
+-- show ########################################################################
+function mod:Show(frame)
     -- Place code to be performed after a frame is shown here.
 end
------------------------------------------------------------------------- Hide --
-function mod:PostHide(msg, frame)
+-- hide ########################################################################
+function mod:Hide(frame)
     -- Place code to be performed after a frame is hidden here.
 end
--------------------------------------------------------------------- Register --
-mod:RegisterMessage('KuiNameplates_PostCreate', 'PostCreate')
-mod:RegisterMessage('KuiNameplates_PostShow', 'PostShow')
-mod:RegisterMessage('KuiNameplates_PostHide', 'PostHide')
+-- initialise ##################################################################
+function mod:Initialise()
+    self:RegisterMessage('Create')
+    self:RegisterMessage('Show')
+    self:RegisterMessage('Hide')
+end
