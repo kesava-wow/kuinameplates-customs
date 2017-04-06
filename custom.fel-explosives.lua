3e73e1313787e308552dec77118c393e3903b857
kesava-wow <kesava-wow@users.noreply.github.com>
2017-04-07 00:20:17 +0100

    make it... nicer?

--- custom.fel-explosives.lua
+++ custom.fel-explosives.lua
@@ -4,41 +4,46 @@ local addon = KuiNameplates
 local core = KuiNameplatesCore
 local mod = addon:NewPlugin('FelExplosives',101)
 
+local function icon_Show(self)
+    self.v:Show()
+    self.h:Show()
+    self.i:Show()
+end
+local function icon_Hide(self)
+    self.v:Hide()
+    self.h:Hide()
+    self.i:Hide()
+end
 function mod:Create(f)
-    local v = f:CreateTexture()
+    f.feicon = {}
+
+    local v = f:CreateTexture(nil,'ARTWORK',nil,1)
     v:SetTexture('interface/buttons/white8x8')
     v:SetVertexColor(1,0,0,.5)
     v:SetHeight(30000)
     v:SetWidth(3)
+    f.feicon.v = v
 
-    local h = f:CreateTexture()
+    local h = f:CreateTexture(nil,'ARTWORK',nil,1)
     h:SetTexture('interface/buttons/white8x8')
     h:SetVertexColor(1,0,0,.5)
     h:SetHeight(3)
     h:SetWidth(30000)
+    f.feicon.h = h
 
-    local i = f:CreateTexture()
+    local i = f:CreateTexture(nil,'ARTWORK',nil,2)
     i:SetTexture(135799)
     i:SetVertexColor(1,1,1,1)
     i:SetHeight(50)
     i:SetWidth(50)
+    f.feicon.i = i
 
     i:SetPoint('BOTTOM',f,'TOP')
     v:SetPoint('CENTER',i)
     h:SetPoint('CENTER',i)
 
-    f.feicon = {
-        ['Show'] = function()
-            v:Show()
-            h:Show()
-            i:Show()
-        end,
-        ['Hide'] = function()
-            v:Hide()
-            h:Hide()
-            i:Hide()
-        end
-    }
+    f.feicon.Show = icon_Show
+    f.feicon.Hide = icon_Hide
     f.feicon:Hide()
 end
 function mod:Show(f)
