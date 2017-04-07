b88fa593691184f477b939a4862e75b59aae6af5
kesava-wow <kesava-wow@users.noreply.github.com>
2017-04-07 12:29:06 +0100

    add cd_text_vertical_offset, auras_vertical_offset

--- custom.simple-aura-mods.lua
+++ custom.simple-aura-mods.lua
@@ -1,15 +1,18 @@
 -- simple-ish modifications for auras
 -- configuration ###############################################################
 local CENTRE_CD_TEXT = true          -- move cooldown text to centre
+--local CD_TEXT_VERTICAL_OFFSET = 8  -- move cooldown text up by 8 pixels
 --local AURA_TEXT_SIZE_MOD = 5       -- increase text size by 5 units
+
+--local AURAS_VERTICAL_OFFSET = -5   -- move aura frame down by 5 pixels
 
 -- changing cooldown text colour:
 local AURA_TEXT_COLOUR_S = {1,0,1}   -- short (<5s) (purple... technically)
-local AURA_TEXT_COLOUR_M = {1,.8,1} -- medium (<20s) (bright pink)
+local AURA_TEXT_COLOUR_M = {1,.8,1}  -- medium (<20s) (bright pink)
 local AURA_TEXT_COLOUR_L = {1,1,1}   -- long (>20s) (white)
 
--- comment out any of the above lines (like this - notice the two dashes at the
--- start) to disable the modification.
+-- delete or comment out any of the above lines (like this - notice the two
+-- dashes at the start) to disable the modification.
 
 -- below lines should be left alone ############################################
 local folder,ns=...
@@ -59,7 +62,16 @@ local function PostCreateAuraButton(frame,button)
     if CENTRE_CD_TEXT then
         -- centre cd text using the callback
         button.cd:ClearAllPoints()
-        button.cd:SetPoint('CENTER',0,core.profile.text_vertical_offset+1)
+
+        if CD_TEXT_VERTICAL_OFFSET then
+            button.cd:SetPoint('CENTER',0,core.profile.text_vertical_offset+CD_TEXT_VERTICAL_OFFSET+1)
+        else
+            button.cd:SetPoint('CENTER',0,core.profile.text_vertical_offset+1)
+        end
+    elseif CD_TEXT_VERTICAL_OFFSET then
+        -- move aura text using callback
+        button.cd:ClearAllPoints()
+        button.cd:SetPoint('TOPLEFT',-2,2+core.profile.text_vertical_offset+CD_TEXT_VERTICAL_OFFSET)
     end
 
     if AURA_TEXT_COLOUR_S or AURA_TEXT_COLOUR_M or AURA_TEXT_COLOUR_L then
@@ -70,8 +82,28 @@ local function PostCreateAuraButton(frame,button)
     end
 end
 
+function mod:Show(f)
+    if AURAS_VERTICAL_OFFSET then
+        -- move aura frame whenever the nameplate is shown
+        -- we're executed after the layout, so this overrides the repositioning it
+        -- does when frames change sizes
+        local af = f.Auras.frames.core_dynamic
+        af:SetPoint(
+            'BOTTOMLEFT',f.bg,'TOPLEFT',
+            floor((f.bg:GetWidth() - af.__width) / 2),
+            15+AURAS_VERTICAL_OFFSET
+        )
+    end
+end
+
 function mod:Initialise()
+    print('|cff9966ffKui Nameplates|r: |cffff6666You are using Kui_Nameplates_Custom which is not updated by the Curse package.|r If you experience errors, check the repository on GitHub for updates.')
+
     self:AddCallback('Auras','PostCreateAuraButton',PostCreateAuraButton)
+
+    if AURAS_VERTICAL_OFFSET then
+        self:RegisterMessage('Show')
+    end
 
     if AURA_TEXT_SIZE_MOD then
         -- modify text size by "overloading" the SetFont helper in core
