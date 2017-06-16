8c556a72d8e95159f939be54616d42460b3e8da3
kesava-wow <kesava-wow@users.noreply.github.com>
2017-06-16 13:35:52 +0100

    replace health text by default, add options

--- custom.two-health-values.lua
+++ custom.two-health-values.lua
@@ -4,14 +4,28 @@ local core = KuiNameplatesCore
 local kui = LibStub('Kui-1.0')
 local mod = addon:NewPlugin('TwoHealthValues',101)
 
-function ReplaceLevelText(f)
-    if f.state.health_cur == f.state.health_max then
-        f.LevelText:SetText()
-    else
-        f.LevelText:SetText(kui.num(f.state.health_cur))
-    end
+-- replace level text instead of modifing health text?
+local REPLACE_LEVEL_TEXT = false
+-- show percent symbol? (only when REPLACE_LEVEL_TEXT is false)
+local PERCENT_SYMBOL = false
 
+-- #############################################################################
+local function ReplaceLevelText(f)
     f:leveltohealth_UpdateHealthText()
+
+    if REPLACE_LEVEL_TEXT then
+        if f.state.health_cur == f.state.health_max then
+            f.LevelText:SetText()
+        else
+            f.LevelText:SetText(kui.num(f.state.health_cur))
+        end
+    else
+        local p = f.state.health_per
+        p = p < 1 and strformat('%.1f',p) or ceil(p)
+
+        -- override with current + percent
+        f.HealthText:SetText(kui.num(f.state.health_cur)..' | '..p..(PERCENT_SYMBOL and '%' or ''))
+    end
 end
 
 function mod:Create(f)
@@ -24,5 +38,8 @@ end
 
 function mod:Initialise()
     self:RegisterMessage('Create')
-    self:RegisterUnitEvent('UNIT_LEVEL')
+
+    if REPLACE_LEVEL_TEXT then
+        self:RegisterUnitEvent('UNIT_LEVEL')
+    end
 end
