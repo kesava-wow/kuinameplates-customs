2e8e1cb88fb939a7ecacb703400eb86c081b9888
kesava-wow <kesava-wow@users.noreply.github.com>
2018-08-07 00:29:09 +0100

    remove text colours, add separate text size modifiers

--- custom.simple-aura-mods.lua
+++ custom.simple-aura-mods.lua
@@ -4,19 +4,15 @@
 
 -- If you want to be able to modify CD text through OmniCC,
 -- uncomment both of these variables:
-local ADD_COOLDOWN_SPIRAL = true      -- add a cooldown spiral to icons
+--local ADD_COOLDOWN_SPIRAL = true    -- add a cooldown spiral to icons
 --local HIDE_CD_TEXT = true           -- hide KNP's CD text
 
 -- Variables after this point obviously won't change anything if you've
 -- enabled "HIDE_CD_TEXT":
-local CENTRE_CD_TEXT = true           -- move cooldown text to centre
+--local CENTRE_CD_TEXT = true         -- move cooldown text to centre
 --local CD_TEXT_VERTICAL_OFFSET = 8   -- move cooldown text up by 8 pixels
---local AURA_TEXT_SIZE_MOD = 5        -- increase text size by 5 units
-
--- changing cooldown text colour:
---local AURA_TEXT_COLOUR_S = {1,0,1}  -- short (<5s) (purple... technically)
---local AURA_TEXT_COLOUR_M = {1,.8,1} -- medium (<20s) (bright pink)
---local AURA_TEXT_COLOUR_L = {1,1,1}  -- long (>20s) (white)
+local CD_TEXT_SIZE_MOD = 2            -- modify cooldown text size
+local COUNT_TEXT_SIZE_MOD = 0         -- modify count text size
 
 -- delete or comment out any of the above lines (like this - notice the two
 -- dashes at the start) to disable the modification.
@@ -28,56 +24,21 @@ local core = KuiNameplatesCore
 local mod = addon:NewPlugin('SimpleAuraMods',101)
 
 local button_UpdateCooldown
-local button_old_UpdateCooldown
-do
-    local old_ou
-    local function button_OnUpdate(self,...)
-        -- OnUpdate is killed when necessary by the original function
-        old_ou(self,...)
-
-        local remaining = self.expiration - GetTime()
-
-        if remaining <= 5 then
-            if AURA_TEXT_COLOUR_S then
-                self.cd:SetTextColor(unpack(AURA_TEXT_COLOUR_S))
-            end
-        elseif remaining <= 20 then
-            if AURA_TEXT_COLOUR_M then
-                self.cd:SetTextColor(unpack(AURA_TEXT_COLOUR_M))
-            end
-        else
-            if AURA_TEXT_COLOUR_L then
-                self.cd:SetTextColor(unpack(AURA_TEXT_COLOUR_L))
-            end
-        end
+local function button_UpdateCooldown(self,duration,expiration)
+    if HIDE_CD_TEXT then
+        self.cd:Hide()
+    else
+        button_old_UpdateCooldown(self,duration,expiration)
     end
 
-    function button_UpdateCooldown(self,duration,expiration)
-        if HIDE_CD_TEXT then
-            self.cd:Hide()
+    if ADD_COOLDOWN_SPIRAL then
+        -- update cooldown spiral
+        if duration and expiration then
+            self.sam_cdf:SetCooldown(expiration-duration,duration)
+            self.sam_cdf:Show()
         else
-            button_old_UpdateCooldown(self,duration,expiration)
-
-            if AURA_TEXT_COLOUR_S or AURA_TEXT_COLOUR_M or AURA_TEXT_COLOUR_L then
-                if expiration and expiration > 0 then
-                    if not old_ou then
-                        old_ou = self:GetScript('OnUpdate')
-                    end
-
-                    self:SetScript('OnUpdate',button_OnUpdate)
-                end
-            end
-        end
-
-        if ADD_COOLDOWN_SPIRAL then
-            -- update cooldown spiral
-            if duration and expiration then
-                self.sam_cdf:SetCooldown(expiration-duration,duration)
-                self.sam_cdf:Show()
-            else
-                self.sam_cdf:SetCooldown(0,0)
-                self.sam_cdf:Hide()
-            end
+            self.sam_cdf:SetCooldown(0,0)
+            self.sam_cdf:Hide()
         end
     end
 end
@@ -115,13 +76,7 @@ local function PostCreateAuraButton(frame,button)
         end
 
         button.cd:SetParent(cdf)
-    end
 
-    if  AURA_TEXT_COLOUR_S or AURA_TEXT_COLOUR_M or AURA_TEXT_COLOUR_L or
-        ADD_COOLDOWN_SPIRAL
-    then
-        -- modify text colour by "overloading" OnUpdate on visible buttons
-        -- (ok, this one isn't so simple until i make this just use a variable)
         button_old_UpdateCooldown = button.UpdateCooldown
         button.UpdateCooldown = button_UpdateCooldown
     end
@@ -150,17 +105,17 @@ function mod:Initialise()
         self:RegisterMessage('Show')
     end
 
-    if AURA_TEXT_SIZE_MOD then
+    if CD_TEXT_SIZE_MOD or COUNT_TEXT_SIZE_MOD then
         -- modify text size by "overloading" the SetFont helper in core
         local old_sf = core.AurasButton_SetFont
         core.AurasButton_SetFont = function(button)
             old_sf(button)
 
             local f,s,t = button.cd:GetFont()
-            button.cd:SetFont(f,s+AURA_TEXT_SIZE_MOD,t)
+            button.cd:SetFont(f,s+CD_TEXT_SIZE_MOD,t)
 
             f,s,t = button.count:GetFont()
-            button.count:SetFont(f,s+AURA_TEXT_SIZE_MOD,t)
+            button.count:SetFont(f,s+COUNT_TEXT_SIZE_MOD,t)
         end
     end
 end
