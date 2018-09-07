546b94b1bd0ab6c302b3771ecb221fe863fa6f12
kesava-wow <kesava-wow@users.noreply.github.com>
2018-09-07 06:02:40 +0100

    update template

--- Kui_Nameplates_Custom/custom.lua
+++ Kui_Nameplates_Custom/custom.lua
@@ -1,27 +1,30 @@
 --[[
 -- Custom code injector template for Kui_Nameplates_Core
--- By Kesava at curse.com
 --]]
 local folder,ns=...
 local addon = KuiNameplates
 local core = KuiNameplatesCore
+
 local mod = addon:NewPlugin('CustomInjector',101)
+if not mod then return end
+
 -- create ######################################################################
 function mod:Create(frame)
     -- Place code to be performed after a frame is created here.
 end
+
 -- show ########################################################################
 function mod:Show(frame)
     -- Place code to be performed after a frame is shown here.
 end
+
 -- hide ########################################################################
 function mod:Hide(frame)
     -- Place code to be performed after a frame is hidden here.
 end
+
 -- initialise ##################################################################
 function mod:Initialise()
-    print('|cff9966ffKui Nameplates|r: |cffff6666You are using Kui_Nameplates_Custom which is not updated by the Curse package.|r If you experience errors, check the repository on GitHub for updates.')
-
     self:RegisterMessage('Create')
     self:RegisterMessage('Show')
     self:RegisterMessage('Hide')
