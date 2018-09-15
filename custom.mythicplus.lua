c524add9cb8394fe898ec4f169538324659d9c18
kesava-wow <kesava-wow@users.noreply.github.com>
2018-09-15 18:35:25 +0100

    add mythicplus

---
+++ custom.mythicplus.lua
@@ -0,0 +1,32 @@
+-- UNTESTED
+-- some icons which are important for mythic+
+-- (they should show up on the top left of nameplates)
+local folder,ns=...
+local addon = KuiNameplates
+local core = KuiNameplatesCore
+
+local mod = addon:NewPlugin('MythicAuras',101,3)
+if not mod then return end
+
+function mod:Create(f)
+    local mythic = f.handler:CreateAuraFrame({
+        max = 2,
+        size = 32,
+        squareness = 1,
+        point = {'BOTTOMRIGHT','RIGHT','LEFT'},
+        rows = 1,
+        filter = 'HELPFUL',
+        whitelist = {
+            [226510] = true, -- sanguine
+            [277242] = true, -- symbiote of g'huun
+        },
+    })
+    mythic:SetFrameLevel(0)
+    mythic:SetWidth(32)
+    mythic:SetHeight(32)
+    mythic:SetPoint('BOTTOMRIGHT',f.bg,'TOPRIGHT',0,10)
+    f.MythicAuras = mythic
+end
+function mod:Initialise()
+    self:RegisterMessage('Create')
+end
