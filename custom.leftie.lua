46bba7dcdc9e174172d29f84afc0032a4e55afce
kesava-wow <kesava-wow@users.noreply.github.com>
2018-11-18 00:24:41 +0000

    generally improve and also fix

--- custom.leftie.lua
+++ custom.leftie.lua
@@ -1,16 +1,12 @@
 local addon = KuiNameplates
 local core = KuiNameplatesCore
 
-local mod = addon:NewPlugin('Leftie',101,4)
+local mod = addon:NewPlugin('Leftie',104,4)
 if not mod then return end
 
-local orig_UpdateHealthText,
-      orig_UpdateNameTextPosition,
-      orig_UpdateSpellNamePosition
-
 -- local functions #############################################################
-local function PostUpdateNameTextPosition(f,...)
-    orig_UpdateNameTextPosition(f,...)
+local function UpdateNameTextPosition(f,...)
+    f.leftie_UpdateNameTextPosition(f,...)
 
     if f.IN_NAMEONLY then return end
 
@@ -24,8 +20,8 @@ local function PostUpdateNameTextPosition(f,...)
         f.NameText:SetPoint('RIGHT',f.HealthBar,2.5,0)
     end
 end
-local function PostUpdateHealthText(f,...)
-    orig_UpdateHealthText(f,...)
+local function UpdateHealthText(f,...)
+    f.leftie_UpdateHealthText(f,...)
 
     if not f.HealthText:IsShown() then return end
     if f.state.no_name then return end
@@ -34,40 +30,35 @@ local function PostUpdateHealthText(f,...)
     f.HealthText:SetJustifyH('RIGHT')
     f.HealthText:SetPoint('BOTTOMRIGHT',f.HealthBar,'TOPRIGHT',-2.5,core.profile.name_vertical_offset)
 end
-local function PostUpdateSpellNamePosition(f,...)
-    orig_UpdateSpellNamePosition(f,...)
+local function UpdateSpellNamePosition(f,...)
+    f.leftie_UpdateSpellNamePosition(f,...)
 
     f.SpellName:ClearAllPoints()
     f.SpellName:SetJustifyH('LEFT')
     f.SpellName:SetPoint(
         'TOPLEFT',f.CastBar,'BOTTOMLEFT',
-        2.5, -2+core.profile.castbar_name_vertical_offset
+        2.5, core.profile.castbar_name_vertical_offset
     )
 end
 -- messages ####################################################################
 function mod:Create(f)
-    if f.NameText then
-        if not orig_UpdateNameTextPosition then
-            orig_UpdateNameTextPosition = f.UpdateNameTextPosition
-        end
-        f.UpdateNameTextPosition = PostUpdateNameTextPosition
+    if f.NameText and not f.leftie_UpdateNameTextPosition then
+        f.leftie_UpdateNameTextPosition = f.UpdateNameTextPosition
+        f.UpdateNameTextPosition = UpdateNameTextPosition
+        f:UpdateNameTextPosition()
     end
-
-    if f.HealthText then
-        if not orig_UpdateHealthText then
-            orig_UpdateHealthText = f.UpdateHealthText
-        end
-        f.UpdateHealthText = PostUpdateHealthText
+    if f.HealthText and not f.leftie_UpdateHealthText then
+        f.leftie_UpdateHealthText = f.UpdateHealthText
+        f.UpdateHealthText = UpdateHealthText
+        f:UpdateHealthText()
     end
-
-    if f.SpellName then
-        if not orig_UpdateSpellNamePosition then
-            orig_UpdateSpellNamePosition = f.UpdateSpellNamePosition
-        end
-        f.UpdateSpellNamePosition = PostUpdateSpellNamePosition
+    if f.SpellName and not f.leftie_UpdateSpellNamePosition then
+        f.leftie_UpdateSpellNamePosition = f.UpdateSpellNamePosition
+        f.UpdateSpellNamePosition = UpdateSpellNamePosition
+        f:UpdateSpellNamePosition()
     end
 end
 -- register ####################################################################
-function mod:Initialise()
+function mod:OnEnable()
     self:RegisterMessage('Create')
 end
