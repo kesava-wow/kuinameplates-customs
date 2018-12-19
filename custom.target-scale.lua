7cc96737873672991889256e94f1cef9125b6a03
kesava-wow <kesava-wow@users.noreply.github.com>
2018-12-19 20:40:45 +0000

    fix targetscale for 81

--- custom.target-scale.lua
+++ custom.target-scale.lua
@@ -3,26 +3,16 @@ local folder,ns=...
 local addon = KuiNameplates
 local core = KuiNameplatesCore
 
-local mod = addon:NewPlugin('TargetScale',101,3)
+local mod = addon:NewPlugin('TargetScale',101,4)
 if not mod then return end
 
 local SCALE_FACTOR = 1.5
 
-local function ForcePositionUpdate(frame)
-    if not frame:IsShown() then return end
-
-    local sizer = _G[frame:GetName()..'PositionHelper']
-    frame:SetPoint('CENTER',WorldFrame,'BOTTOMLEFT',
-        floor(sizer:GetWidth()),floor(sizer:GetHeight()))
-end
-
 function mod:GainedTarget(frame)
     frame:SetScale(UIParent:GetScale()*SCALE_FACTOR)
-    ForcePositionUpdate(frame)
 end
 function mod:LostTarget(frame)
     frame:SetScale(UIParent:GetScale())
-    ForcePositionUpdate(frame)
 end
 function mod:Initialise()
     self:RegisterMessage('GainedTarget')
